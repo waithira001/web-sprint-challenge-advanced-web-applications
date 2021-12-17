e3e3378d05b3381887fd58380d501185a6e5b24c
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-17 17:09:58 -0600

    Delete View.test.js

--- src/components/View.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import View from './View';
-
-test("renders zero articles without errors", async () => {
-});
-
-// test("renders three articles without errors", async ()=> {
-// });
