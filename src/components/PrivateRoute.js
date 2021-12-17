5f28e030c16ee4a8bbade12faaa4067c6f1b6928
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-17 17:09:46 -0600

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,22 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
-function PrivateRoute({component:Component,...rest}) {
-  return (<Route
-  
-    {...rest}
-    render={props =>
-      localStorage.getItem("token") ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
+function PrivateRoute({component:Component, ...rest}) {
+  return (<Route {...rest} render={(props) => {
+    if(localStorage.getItem("token")) {
+      return <Component {...props} />
+    } else {
+      <Redirect to="/login" />
     }
-  
-  />);
+  }}/>);
 }
 
 export default PrivateRoute;
+
 
 //Task List:
 //1. Complete PrivateRoute
